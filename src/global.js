fbc6b42ec019fe9b51f907b643a23f6889704a92
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-10-05 21:28:14 +0530

    updated API & socketAPI

--- src/global.js
+++ src/global.js
@@ -1,3 +1,5 @@
-export const API = "http://localhost:5000";
+export const API = "https://chation-backend.herokuapp.com";
+// export const API = "http://localhost:5000";
 
-export const socketAPI = "http://localhost:5000";
+export const socketAPI = "https://chation-backend.herokuapp.com";
+// export const socketAPI = "http://localhost:5000";
